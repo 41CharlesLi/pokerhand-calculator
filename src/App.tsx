8997188dcdcdf3ff928a5e3c65f970c52541e6d0
Charles Li <96878550+41CharlesLi@users.noreply.github.com>
2022-10-21 10:13:20 -0400

    created data for community cards + created communityCards state

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import CommunitySelect from "./components/CommunitySelect";
+import { useState } from "react";
+
 function App() {
-    return <div className="App"></div>;
+    const [communityCards, setCommunityCards] = useState<string[]>([]);
+    console.log(communityCards);
+    return (
+        <div className="App">
+            <CommunitySelect setCommunityCards={setCommunityCards} />
+        </div>
+    );
 }
 
 export default App;
+
+//have community card state
+//
