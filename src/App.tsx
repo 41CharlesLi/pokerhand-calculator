8d92e96e4807cb0f5106223be3dc20b56a7bb1a9
Charles Li <96878550+41CharlesLi@users.noreply.github.com>
2022-10-22 11:57:00 -0400

    limited player count to 9

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import CommunitySelect from "./components/CommunitySelect";
 import { useState } from "react";
+import Addplayer from "./components/Addplayer";
 
 function App() {
     const [communityCards, setCommunityCards] = useState<string[]>([]);
+    const [playerCards, setPlayerCards] = useState<object[]>([
+        {
+            player: 1,
+            hand: [],
+        },
+        { player: 2, hand: [] },
+    ]);
+
+    console.log({ playerCards });
     console.log(communityCards);
     return (
         <div className="App">
             <CommunitySelect setCommunityCards={setCommunityCards} />
+            <Addplayer
+                playerCards={playerCards}
+                setPlayerCards={setPlayerCards}
+            />
         </div>
     );
 }
