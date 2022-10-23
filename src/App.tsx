0ca6f23f55d5a11bfd27bd2a548aa5bc97b3e826
Charles Li <96878550+41CharlesLi@users.noreply.github.com>
2022-10-23 12:24:51 -0400

    finished communitySelect component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,71 @@
 import CommunitySelect from "./components/CommunitySelect";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Addplayer from "./components/Addplayer";
 
 interface Player {
     player: number;
     hand: string[];
 }
+interface Card {
+    value: string;
+    label: string;
+    player: string;
+}
+const cardData = [
+    { value: "AS", label: "Ace of Spade", player: "" },
+    { value: "AH", label: "Ace of Hearts", player: "" },
+    { value: "AC", label: "Ace of Clover", player: "" },
+    { value: "AD", label: "Ace of Diamonds", player: "" },
+    { value: "KS", label: "King of Spade", player: "" },
+    { value: "KH", label: "King of Hearts", player: "" },
+    { value: "KC", label: "King of Clover", player: "" },
+    { value: "KD", label: "King of Diamonds", player: "" },
+    { value: "QS", label: "Queen of Spade", player: "" },
+    { value: "QH", label: "Queen of Hearts", player: "" },
+    { value: "QC", label: "Queen of Clover", player: "" },
+    { value: "QD", label: "Queen of Diamonds", player: "" },
+    { value: "JS", label: "Jack of Spade", player: "" },
+    { value: "JH", label: "Jack of Hearts", player: "" },
+    { value: "JC", label: "Jack of Clover", player: "" },
+    { value: "JD", label: "Jack of Diamonds", player: "" },
+    { value: "10S", label: "10 of Spade", player: "" },
+    { value: "10H", label: "10 of Hearts", player: "" },
+    { value: "10C", label: "10 of Clover", player: "" },
+    { value: "10D", label: "10 of Diamonds", player: "" },
+    { value: "9S", label: "9 of Spade", player: "" },
+    { value: "9H", label: "9 of Hearts", player: "" },
+    { value: "9C", label: "9 of Clover", player: "" },
+    { value: "9D", label: "9 of Diamonds", player: "" },
+    { value: "8S", label: "8 of Spade", player: "" },
+    { value: "8H", label: "8 of Hearts", player: "" },
+    { value: "8C", label: "8 of Clover", player: "" },
+    { value: "8D", label: "8 of Diamonds", player: "" },
+    { value: "7S", label: "7 of Spade", player: "" },
+    { value: "7H", label: "7 of Hearts", player: "" },
+    { value: "7C", label: "7 of Clover", player: "" },
+    { value: "7D", label: "7 of Diamonds", player: "" },
+    { value: "6S", label: "6 of Spade", player: "" },
+    { value: "6H", label: "6 of Hearts", player: "" },
+    { value: "6C", label: "6 of Clover", player: "" },
+    { value: "6D", label: "6 of Diamonds", player: "" },
+    { value: "5S", label: "5 of Spade", player: "" },
+    { value: "5H", label: "5 of Hearts", player: "" },
+    { value: "5C", label: "5 of Clover", player: "" },
+    { value: "5D", label: "5 of Diamonds", player: "" },
+    { value: "4S", label: "4 of Spade", player: "" },
+    { value: "4H", label: "4 of Hearts", player: "" },
+    { value: "4C", label: "4 of Clover", player: "" },
+    { value: "4D", label: "4 of Diamonds", player: "" },
+    { value: "3S", label: "3 of Spade", player: "" },
+    { value: "3H", label: "3 of Hearts", player: "" },
+    { value: "3C", label: "3 of Clover", player: "" },
+    { value: "3D", label: "3 of Diamonds", player: "" },
+    { value: "2S", label: "2 of Spade", player: "" },
+    { value: "2H", label: "2 of Hearts", player: "" },
+    { value: "2C", label: "2 of Clover", player: "" },
+    { value: "2D", label: "2 of Diamonds", player: "" },
+];
+
 function App() {
     const [communityCards, setCommunityCards] = useState<string[]>([]);
     const [playerCards, setPlayerCards] = useState<Player[]>([
@@ -15,10 +75,16 @@ function App() {
         },
         { player: 2, hand: [] },
     ]);
-
+    const [cardDeck, setCardDeck] = useState<Card[]>(cardData);
+    console.log(communityCards);
     return (
         <div className="App">
-            <CommunitySelect setCommunityCards={setCommunityCards} />
+            <h1>Poker Hand Calculator</h1>
+            <CommunitySelect
+                setCommunityCards={setCommunityCards}
+                cardDeck={cardDeck}
+                setCardDeck={setCardDeck}
+            />
             <Addplayer
                 playerCards={playerCards}
                 setPlayerCards={setPlayerCards}
@@ -28,6 +94,3 @@ function App() {
 }
 
 export default App;
-
-//have community card state
-//
--- src/components/CommunitySelect.tsx
+++ src/components/CommunitySelect.tsx
@@ -1,77 +1,53 @@
 import React from "react";
 import { MultiSelect } from "@mantine/core";
 
+interface Card {
+    value: string;
+    label: string;
+    player: string;
+}
+
 type CommunitySelectProps = {
     setCommunityCards: React.Dispatch<React.SetStateAction<string[]>>;
+    cardDeck: Card[];
+    setCardDeck: React.Dispatch<React.SetStateAction<Card[]>>;
 };
 
-const cardData = [
-    { value: "AS", label: "Ace of Spade" },
-    { value: "AH", label: "Ace of Hearts" },
-    { value: "AC", label: "Ace of Clover" },
-    { value: "AD", label: "Ace of Diamonds" },
-    { value: "KS", label: "King of Spade" },
-    { value: "KH", label: "King of Hearts" },
-    { value: "KC", label: "King of Clover" },
-    { value: "KD", label: "King of Diamonds" },
-    { value: "QS", label: "Queen of Spade" },
-    { value: "QH", label: "Queen of Hearts" },
-    { value: "QC", label: "Queen of Clover" },
-    { value: "QD", label: "Queen of Diamonds" },
-    { value: "JS", label: "Jack of Spade" },
-    { value: "JH", label: "Jack of Hearts" },
-    { value: "JC", label: "Jack of Clover" },
-    { value: "JD", label: "Jack of Diamonds" },
-    { value: "10S", label: "10 of Spade" },
-    { value: "10H", label: "10 of Hearts" },
-    { value: "10C", label: "10 of Clover" },
-    { value: "10D", label: "10 of Diamonds" },
-    { value: "9S", label: "9 of Spade" },
-    { value: "9H", label: "9 of Hearts" },
-    { value: "9C", label: "9 of Clover" },
-    { value: "9D", label: "9 of Diamonds" },
-    { value: "8S", label: "8 of Spade" },
-    { value: "8H", label: "8 of Hearts" },
-    { value: "8C", label: "8 of Clover" },
-    { value: "8D", label: "8 of Diamonds" },
-    { value: "7S", label: "7 of Spade" },
-    { value: "7H", label: "7 of Hearts" },
-    { value: "7C", label: "7 of Clover" },
-    { value: "7D", label: "7 of Diamonds" },
-    { value: "6S", label: "6 of Spade" },
-    { value: "6H", label: "6 of Hearts" },
-    { value: "6C", label: "6 of Clover" },
-    { value: "6D", label: "6 of Diamonds" },
-    { value: "5S", label: "5 of Spade" },
-    { value: "5H", label: "5 of Hearts" },
-    { value: "5C", label: "5 of Clover" },
-    { value: "5D", label: "5 of Diamonds" },
-    { value: "4S", label: "4 of Spade" },
-    { value: "4H", label: "4 of Hearts" },
-    { value: "4C", label: "4 of Clover" },
-    { value: "4D", label: "4 of Diamonds" },
-    { value: "3S", label: "3 of Spade" },
-    { value: "3H", label: "3 of Hearts" },
-    { value: "3C", label: "3 of Clover" },
-    { value: "3D", label: "3 of Diamonds" },
-    { value: "2S", label: "2 of Spade" },
-    { value: "2H", label: "2 of Hearts" },
-    { value: "2C", label: "2 of Clover" },
-    { value: "2D", label: "2 of Diamonds" },
-];
+function CommunitySelect({
+    setCommunityCards,
+    cardDeck,
+    setCardDeck,
+}: CommunitySelectProps) {
+    const handleCardSelect = (cardDeck: Card[], cards: string[]) => {
+        const cardMap = cardDeck.map((card) => {
+            if (cards.includes(card.value)) {
+                card.player = "community";
+            } else {
+                card.player = "";
+            }
+            return card;
+        });
 
-function CommunitySelect({ setCommunityCards }: CommunitySelectProps) {
+        setCardDeck(cardMap);
+        setCommunityCards([...cards]);
+    };
+
+    const availableCards = cardDeck.filter((card) => {
+        return card.player !== ("commmunity" || "");
+    });
+
+    console.log({ cardDeck, availableCards });
     return (
         <div>
             <MultiSelect
-                data={cardData}
+                data={availableCards}
                 label="Community Cards"
                 placeholder="Select your community cards"
                 searchable
                 nothingFound="Nothing found"
                 maxSelectedValues={5}
                 clearable
-                onChange={(string) => setCommunityCards([...string])}
+                onChange={(string) => handleCardSelect(cardDeck, string)}
             />
         </div>
     );
